ec9975ee30efb00ffdf6a5276b5570431e006e5b
fraserxu <xvfeng123@gmail.com>
2014-01-16 00:13:24 +0800

    add dummy test

---
+++ test/test.js
@@ -0,0 +1,32 @@
+var player = require('..')
+// var test = require('tape')
+
+// test('search', function(t) {
+//   t.plan(1);
+
+  player.search('Love Story', function(err, res) {
+    if(err) console.log(err);
+    console.log('search result: ', res)
+    // t.equal(typeof res, 'object')
+  })
+// })
+
+// test('album', function(t) {
+//   t.plan(1);
+
+  // player.album('23497', function(err, res) {
+  //   if(err) console.log(err);
+  //   console.log('album info: ', res)
+  //   // t.equal(typeof res, 'object')
+  // })
+// })
+
+// test('detail', function(t) {
+//   t.plan(1);
+
+  // player.detail('233931', function(err, res) {
+  //   if(err) console.log(err);
+  //   console.log('song info: ', res)
+  //   // t.equal(typeof res, 'object')
+  // })
+// })
